7017c7b51dc7b723bf2d5b0491e7529ac426fe13
Alexis Sellier <biopunk@gmail.com>
2009-02-04 14:20:38 +0100

    reduced sloc

--- spell-correct.rb
+++ spell-correct.rb
@@ -1,30 +1,27 @@
+#
+# A Ruby Spell Corrector in 19 sloc. Based on Peter Norvig's python spell corrector
+# 
+LETTERS = ("a".."z").to_a
 NWORDS = Hash.new( 1 )
-
 File.new('holmes.txt').read.downcase.scan(/[a-z]+/) { |k| NWORDS[k] += 1 }
-
-LETTERS = ("a".."z").to_a.join
 
 def edit word
     n = word.length
-    alteration = insertion = []
   
-    deletion = (0...n).collect do |i| word[0...i] + word[i + 1..-1] end                                          # Deletion
-    transposition = (0...n-1).collect do |i| word[0...i] + word[i + 1, 1] + word[i, 1] + word[i + 2..-1] end     # Transposition
-    n.times do |i| LETTERS.each_byte do |l| alteration << word[0...i] + l.chr + word[i + 1..-1] end end          # Alterations
-    (n + 1).times do |i| LETTERS.each_byte do |l| insertion << word[0...i] + l.chr + word[i..-1] end end         # Insertion
-  
-    if ! ( result = deletion + transposition + alteration + insertion ).empty? then result end
+    (result = (0...n).collect { |i| word[0...i] + word[i + 1..-1] } +                                                      # Deletion
+    (0...n-1).collect { |i| word[0...i] + word[i + 1, 1] + word[i, 1] + word[i + 2..-1] } +                                # Transposition    
+    (n).times.collect { |i| LETTERS.collect { |l| word[0...i] + l.chr + word[i + 1..-1] } }.flatten +                      # Alteration
+    (n + 1).times.collect { |i| LETTERS.collect { |l| word[0...i] + l.chr + word[i..-1] } }.flatten).empty? ? nil : result # Insertion  
 end
 
 def edit2 word
-    edit( word ).collect do |e| known( edit( e ) ) end.reject do |v| !v end
+    edit( word ).collect { |e| known( edit( e ) ) }.reject { |v| !v }
 end
 
 def known words
-    if ! ( result = words.select do |w| NWORDS.has_key?(w) end ).empty? then result end
+    ( result = words.select { |w| NWORDS.has_key?(w) } ).empty? ? nil : result
 end
 
 def correct word
-    ( known([word]) || known(edit(word)) || edit2(word) || word ).first.max do |a, b| 
-        NWORDS[a] <=> NWORDS[b] end || nil
+    ( known([word]) || known(edit(word)) || edit2(word) || word ).uniq.max do |a, b| NWORDS[a] <=> NWORDS[b] end
 end
